32882d381220a5ea3b458036dd98e78ad02df2c2
Ivan Palomares <shades1017@gmail.com>
2017-09-12 08:35:52 -0500

    12th trying

--- config/views.py
+++ config/views.py
@@ -22,7 +22,7 @@ class UserViewSet(viewsets.ModelViewSet):
     def create_user(self, request, pk=None):
         serializer = UserSerializer(data=request.data)
         try:
-            user = User.objects.get(name=serializer.data.get('name'))
+            user = User.objects.get(name=serializer.__getitem__('name').value)
             return Response(user)
         except User.DoesNotExist:
             if(serializer.is_valid()):
